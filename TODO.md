e7282d7bf0e6d0f4c730957618302e563d36f9b4
Remy Szpuk (Salon Luxe) <remy.szpuk@salonluxe.co.uk>
2021-02-19 20:41:46 +0000

    Removed /pycache/ Added lib.wilsonDB
    Removed all instances of /pycache/
    Added lib.wilsonDB
    Removed contents of lib.data.py
    Added __init__.py to /lib/

--- TODO.md
+++ TODO.md
@@ -1,1 +1,12 @@
 # TODO
+Todo list for the Wilson bot.
+
+## Database
+
+## Datasets
+
+## GitHub 
+
+## Reddit
+
+## Loki
---
+++ lib/__init__.py
@@ -0,0 +1,10 @@
+"""
+lib: library folder contains all external libraries used in commands.
+This helps keep commands small which improves readability.
+
+Todo list:
+- loki guild manager: gives guilds different commands depending on what flags are given upon init.
+- github wrapper: basic wrapper around PyGitHub to access specific repositories.
+- wilsonDB: basic database manager
+
+"""
