227058cf3556a5725a6102d678efe5a8234191a1
Remy Szpuk <elijoffee@gmail.com>
2021-02-28 16:30:35 +0000

    Started rock paper scissors

---
+++ extensions/rock_paper_scissors.py
@@ -0,0 +1,64 @@
+import discord 
+from discord.ext import commands
+
+class RockPaperScissors(commands.Cog, name="Rock Paper Scissors"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.emojis = ["🪨","📄","✂️","❌"]
+    
+    @commands.group(
+        pass_context = True,
+        name="Rock Paper Scissors",
+        aliases=["rps"]
+    )
+    async def rock_paper_scissor(self, ctx):
+        if ctx.invoked_subcommand is None:
+            # Error
+            pass
+    
+    @rock_paper_scissor.command(
+        pass_context = True,
+        aliases=["fight"],
+        name="challenge"
+    )
+    async def challenge(self, ctx, user):
+        challenger = self.bot.get_user(ctx.author.id)
+        reciever = self.bot.get_user(user)
+        embed = discord.Embed(colour=discord.Colour.yellow())
+        embed.set_author(name="Rock! Paper! Scissors!", icon_url=ctx.author.avatar_url)
+        embed.add_field(
+            name=f"{ctx.author.name} challenged you to rock paper scissors!",
+            value=f"Respond with 'rock', 'paper' or 'scissors'! You can also submit 'decline' to decline the match."
+        )
+        embed.add_field(
+            name=f"How do I respond?",
+            value=f"Click on the emoji which you want your answer to be. If you don't want to respond click on the red X."
+        )
+        _embed = discord.Embed(colour=discord.Colour.purple())
+        _embed.set_author(name="Rock! Paper! Scissors!", icon_url=ctx.author.avatar_url)
+        _embed.add_field(
+            name=f"You challenged {reciever.name} to rock paper scissors!",
+            value=f"Respond with 'rock', 'paper' or 'scissors'! You can also submit 'decline' to decline the match."
+        )
+        _embed.add_field(
+            name=f"How do I respond?",
+            value=f"Click on the emoji which you want your answer to be. If you don't want to respond click on the red X."
+        )
+        msg = await challenger.send(embed=_embed)
+        _msg = await reciever.send(embed=embed)
+        for emoji in self.emojis:
+            await msg.add_reaction(emoji)
+            await _msg.add_reaction(emoji)
+
+        @commands.Cog.listener()
+        async def on_reaction_add(self, ctx):
+            pass
+        
+    
+    
+
+
+    
+
+def setup(bot):
+    bot.add_cog(RockPaperScissors(bot))
