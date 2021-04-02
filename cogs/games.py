54e4c074267848b55c49f30d4fc447d7127054fb
Remy <elijoffee@gmail.com>
2021-04-02 16:18:43 +0100

    Testing coin flip bet. Next: Add economy and database

--- cogs/games.py
+++ cogs/games.py
@@ -37,12 +37,16 @@ class CoinFlip(commands.Cog, name="Coin Flip"):
                 )
             await ctx.send(embed=embed)
 
-    @coin_flip.command(
+    @coin_flip.group(
         name="bet",
         pass_context=True
     )
-    async def coin_flip_bet(self, ctx, bet_amount: int):
-        pass
+    async def coin_flip_bet(self, ctx, bet_id=None):
+        if ctx.invoked_subcommand is None and bet_id is None:
+            await ctx.send("Need subcommand soz bro")
+        elif bet_id is not None:
+            pass
+            # Add bet to stack
         """
         ~ TODO ~
         1. Create bet stack
@@ -56,6 +60,27 @@ class CoinFlip(commands.Cog, name="Coin Flip"):
         9. Add money to winner balance
         10. delete stack
         """
+
+    @coin_flip_bet.command(
+        name="start",
+        pass_context=True
+    )
+    async def coin_flip_bet_start(self, ctx, bet_amount: int):
+        self.stack.set_author(ctx.author.id)
+        embed = discord.Embed(colour=discord.Colour.random())
+        embed.set_author(
+            name=f"{ctx.author.name} has started a bet!",
+            icon_url="https://pics.freeicons.io/uploads/icons/png/10346513851578289009-512.png"
+        )
+        embed.add_field(name="Bet type", value="Coin flip (Heads or Tails)", inline=False)
+        embed.add_field(
+            name="Bet is live!",
+            value=f"You have 60 seconds to submit your bet!\nYou can submit a bet using ::coinflip bet <amount>!"
+        )
+        await ctx.send(embed=embed)
+        while self.stack.start_timer(seconds=60) == "Finished":
+            await ctx.send("Bets are in.")
+
 
 
 
