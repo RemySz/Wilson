263f60a4fe6085fcbcf25926632def9c7b404190
Remy <elijoffee@gmail.com>
2021-04-10 04:18:40 +0100

    Added plan for data Added configs

--- lib/data.py
+++ lib/data.py
@@ -1,12 +1,41 @@
+"""
+For managing and scraping data from json files.
+
+Classes:
+    DataStream:
+        inputting/outputting data from a specific file
+
+    DataSearch:
+        finding a specific file in a directory
+
+    DataBase:
+        for managing a large set of files
+"""
 
 
 class DataSteam:
-    """"""
+    """
+
+    """
     def __init__(self, file=None):
         self.file = file
         self.contents = ""
         with open(self.file, 'r') as f:
             self.contents = f.read()
+
+
+class DataSearch:
+    """
+
+    """
+    pass
+
+
+class DataBase:
+    """
+
+    """
+    pass
 
 
 
