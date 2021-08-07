20a4a124c413b5b218b3481658810cfbe0d6dd26
tokorv <tokorv.studios@gmail.com>
2021-08-07 01:37:15 +0100

    recollection

--- wilson.py
+++ wilson.py
@@ -1,20 +1,12 @@
 import discord
 from discord.ext import commands
 
-bot = discord.Client()
-bot = commands.Bot(command_prefix="!!", description="Wilson bot")
 
-# Load core
-core = ["tictactoe"]
-for member in core:
-    bot.load_extension(f"core.{member}")
+def runner():
+    bot = commands.Bot(command_prefix="!!", description="Wilson bot")
+    print("Ready")
+    bot.run("NzYyNjk0NDY1MjYxMjczMTI4.X3s4kg.-7nmyqjRGHtTnioUQ958eES69YY")
 
-# Load extenstions
 
-try:
-    with open(".token", 'r') as f:
-        bot.run(f.read())
-except FileNotFoundError:
-    print(".token was not found")
-    print("Please generate a bot token and place it inside a .token file")
-    exit(1)
+runner()
+
